b4740a57f42f883aec88b7412f2e319b507c4e4c
Win <synwe@yahoo.com>
2019-02-14 18:45:24 +0000

    Add example

---
+++ example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:geopoint/geopoint.dart';
+
+class _MyHomePageState extends State<MyHomePage> {
+  GeoPoint geoPoint;
+
+  setGeoPoint() {
+    getGeoPoint(verbose: true).then((p) {
+      setState(() {
+        geoPoint = p;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Get current geopoint"),
+                onPressed: () => setGeoPoint(),
+              ),
+              Text("$geoPoint"),
+            ],
+          )),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Geopoint example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
