75a66075761f2ff481955d033df528e949c8f530
Win <synwe@yahoo.com>
2019-07-15 16:57:46 +0000

    Add an example

---
+++ example/main.dart
@@ -0,0 +1,34 @@
+import 'package:geopoint/geopoint.dart';
+import 'package:latlong/latlong.dart';
+
+class Place {
+  Place(this.name, this.point);
+
+  final String name;
+  final LatLng point;
+}
+
+final List<Place> places = [
+  Place("Notre-Dame", LatLng(48.853831, 2.348722)),
+  Place("Montmartre", LatLng(48.886463, 2.341169)),
+  Place("Champs-Elysées", LatLng(48.873932, 2.294821)),
+  Place("Chinatown", LatLng(48.827393, 2.361897)),
+  Place("Tour Eiffel", LatLng(48.85801, 2.294713))
+];
+
+void main() async {
+  final geoPoints = <GeoPoint>[];
+  print("Places in Paris:");
+  for (final place in places) {
+    final geoPoint = GeoPoint.fromLatLng(name: place.name, point: place.point);
+    print("${geoPoint.toMap()}");
+    geoPoints.add(geoPoint);
+  }
+  print("Geoserie:");
+  final geoSerie = GeoSerie(
+      name: "Paris places", type: GeoSerieType.group, geoPoints: geoPoints);
+  print("${geoSerie.toMap()}");
+  for (final gp in geoSerie.geoPoints) {
+    print("${gp.name}: ${gp.latitude}/${gp.longitude}");
+  }
+}
